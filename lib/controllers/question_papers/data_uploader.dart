8f0b430c79388194e84b834ba0cfb1e79aa9b4ac
ryan30sep <riyan30sep@gmail.com>
2024-05-30 08:45:17 +0700

    commit

---
+++ lib/controllers/question_papers/data_uploader.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_quiz_app/models/question_paper_model.dart';
+import 'package:get/get.dart';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DataUploader extends GetxController {
+  @override 
+  void onReady() {
+    uploadData();
+    super.onReady();
+  }
+
+  Future <void> uploadData() async {
+    final fireStore = FirebaseFirestore.instance;
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final papersInAssets = manifestMap.keys
+        .where((path)=>
+            path.startsWith("assets/DB/paper")&&path.contains(".json"))
+        .toList();
+      List<QuestionPaperModel> questionPapers = [];
+      for (var paper in papersInAssets) {
+        String stringPaperContent = await rootBundle.loadString(paper);
+        questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+      }
+      //print ('items number ${questionPapers[0].description}');
+      var batch = fireStore.batch();
+  }
+}
---
+++ lib/data_uploader_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_quiz_app/controllers/question_papers/data_uploader.dart';
+import 'package:get/get.dart';
+
+class DataUploaderScreen extends StatelessWidget {
+  DataUploaderScreen({Key? key}) : super(key: key);
+  DataUploader controller = Get.put(DataUploader());
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold (
+        body:Center(
+      child: Text("Uploading"),
+      ));
+  }
+}
